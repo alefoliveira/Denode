282e89167a33c50a20c9f25ca0b1e4b440563027
erikacs30 <erika.cs30@gmail.com>
2017-06-14 16:06:17 -0300

    Add files via upload

--- apoio/insere_atividades_selecionadas.php
+++ apoio/insere_atividades_selecionadas.php
@@ -5,26 +5,38 @@ require '../config.php';
 //INSERE ATIVIDADES SELECIONADAS NO BANCO
 if (isset($_POST['cadastrar'])) {
 	
-	$atividades = "SELECT `ATIVIDADES_ATIEMPPLA`, `STATUS_ATIEMPPLA`, `ID_EMP` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+	
 
 	$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
 
 	mysql_select_db($banco);
+
+	$atividades = "SELECT `ATIVIDADES_ATIEMPPLA`, `STATUS_ATIEMPPLA`, `ID_EMP` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+	$registros = mysql_num_rows($query);
 	
 	if(isset($_POST['atividades'])){
-	 
+		 		 
 	 	$listaAtividades = $_POST['atividades'];
 	 	$atividadesSelecionadas = implode(",",$listaAtividades);
 	    
+	    if(!isset($registros)){
 
-		$sqlAti = "UPDATE `atividades_empresa_plat` SET `ATIVIDADES_ATIEMPPLA`='1' WHERE `ID_EMP` = 1";//"INSERT INTO ATIVIDADES_EMPRESA_PLAT (ATIVIDADES_ATIEMPPLA, STATUS_ATIEMPPLA, ID_EMP) VALUES ('". $atividadesSelecionadas . "','0',1)"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
-		$queryAti = mysql_query($sqlAti, $conexao);
+			$sqlAti = "UPDATE `atividades_empresa_plat` SET `ATIVIDADES_ATIEMPPLA`='" . $atividadesSelecionadas . "' WHERE `ID_EMP` = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+			$queryAti = mysql_query($sqlAti, $conexao);
 
-		header("Location: ../selecao_atividades.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
+			header("Location: ../selecao_atividades.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
 			
+		} else {
+			
+			$sqlAti = "INSERT INTO ATIVIDADES_EMPRESA_PLAT (ATIVIDADES_ATIEMPPLA, STATUS_ATIEMPPLA, ID_EMP) VALUES ('". $atividadesSelecionadas . "','0',1)"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+			$queryAti = mysql_query($sqlAti, $conexao);
+
+			header("Location: ../selecao_atividades.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
+		}	
 	} else {
 		echo 'Voce nao selecionou nenhuma atividade ';
 	}
+	
 
 }
 ?>
