838ac8f8e461153f7d3a876373ed9969fac1a1a6
erikacs30 <erika.cs30@gmail.com>
2017-06-14 22:40:52 -0300

    Add files via upload

--- apoio/insere_atividades_selecionadas.php
+++ apoio/insere_atividades_selecionadas.php
@@ -12,27 +12,31 @@ if (isset($_POST['cadastrar'])) {
 	mysql_select_db($banco);
 
 	$atividades = "SELECT `ATIVIDADES_ATIEMPPLA`, `STATUS_ATIEMPPLA`, `ID_EMP` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
-	$registros = mysql_num_rows($query);
+	
+	$queryAti = mysql_query($atividades, $conexao);
+	$registros = mysql_num_rows($queryAti);
+
 	
 	if(isset($_POST['atividades'])){
 		 		 
 	 	$listaAtividades = $_POST['atividades'];
 	 	$atividadesSelecionadas = implode(",",$listaAtividades);
 	    
-	    if(!isset($registros)){
+	    if($registros != "0"){
 
 			$sqlAti = "UPDATE `atividades_empresa_plat` SET `ATIVIDADES_ATIEMPPLA`='" . $atividadesSelecionadas . "' WHERE `ID_EMP` = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
-			$queryAti = mysql_query($sqlAti, $conexao);
-
-			header("Location: ../selecao_atividades.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
+			
 			
 		} else {
 			
 			$sqlAti = "INSERT INTO ATIVIDADES_EMPRESA_PLAT (ATIVIDADES_ATIEMPPLA, STATUS_ATIEMPPLA, ID_EMP) VALUES ('". $atividadesSelecionadas . "','0',1)"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
-			$queryAti = mysql_query($sqlAti, $conexao);
-
-			header("Location: ../selecao_atividades.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
+			
 		}	
+
+		$queryAti = mysql_query($sqlAti, $conexao);
+
+		header("Location: ../selecao_atividades.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
+
 	} else {
 		echo 'Voce nao selecionou nenhuma atividade ';
 	}
