1dcf7bbff07853f464d95d4e9fc1f5528ef3419b
erikacs30 <erika.cs30@gmail.com>
2017-06-29 22:32:04 -0300

    290604

--- apoio/insere_cronograma_empresa.php
+++ apoio/insere_cronograma_empresa.php
@@ -7,26 +7,46 @@ if (isset($_POST['cadastrar'])) {
 
 	$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
 	mysql_select_db($banco);
-	$atividadesCro = "SELECT `ATIVIDADES_CROEMPPLA`, `ID_EMP`, `INICIO_CROEMPPLA`, `FIM_CROEMPPLA` from CRONOGRAMA_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+	$atividadesCro = "SELECT `ID_EMP` from CRONOGRAMA_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
 	$queryAtiCro = mysql_query($atividadesCro, $conexao);
 	$registros = mysql_num_rows($queryAtiCro);
 
 	
 	if(isset($_POST['atividades'])){
 		 		 
-	 	$listaatividadesCro = $_POST['atividades'];
-	 	$atividadesSelecionadas = implode(",",$listaatividadesCro);
+	 	$listaAtividadesCro = $_POST['atividades'];
+	 	$atividadesSelecionadas = implode(",",$listaAtividadesCro);
+	 	$listaDiasCro = $_POST['dias'];
+	 	$diasSelecionados = implode(",",$listaDiasCro);
+	 	$listaColabCro = $_POST['colaboradores'];
+	 	$colabSelecionados = implode(",",$listaColabCro);
+	 	
+	 	if (!isset($_POST['ativo'])){$ativo = 0;} else {
+	 	$listaAtivos = $_POST['ativo'];
+	 	$ativo = implode(",",$listaAtivos);
+	 		
+	 	}
+
+
+
+
+	 	echo $colabSelecionados;
 	    
-	    if($registros != "0"){
-			$sqlAtiCro = "UPDATE `CRONOGRAMA_EMPRESA_PLAT` SET `ATIVIDADES_CROEMPPLA`='" . $atividadesSelecionadas . "',`INICIO_CROEMPPLA`='" . $_POST['inicio'] . "',`FIM_CROEMPPLA`='" . $_POST['termino'] . ",`ATIVO_CROEMPPLA`='" . $_POST['ativo'] . " WHERE `ID_EMP` = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+	   /* if($registros != "0"){
+			
+	    	echo 'tem registros';
+			$sqlAtiCro = "UPDATE `CRONOGRAMA_EMPRESA_PLAT` SET `ATIVIDADES_CROEMPPLA`='" . $atividadesSelecionadas . "',`INICIO_CROEMPPLA`='" . $_POST['inicio'] . "',`FIM_CROEMPPLA`='" . $_POST['termino'] . ",`ATIVO_CROEMPPLA`='" . $_POST['ativo'] . ",`PARTICIPANTES_CROEMPPLA`='". $colabSelecionados . "',`DIAS_CROEMPPLA`='" . $diasSelecionados . "' WHERE `ID_EMP` = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
 			//,`PARTICIPANTES_CROEMPPLA`='" . $atividadesSelecionadas . "
 		} else {
-			$sqlAtiCro = "INSERT INTO CRONOGRAMA_EMPRESA_PLAT (`ATIVIDADES_CROEMPPLA`, `INICIO_CROEMPPLA`, `FIM_CROEMPPLA`, `ATIVO_CROEMPPLA`) VALUES ('". $atividadesSelecionadas . "','" . $_POST['inicio'] . "','" . $_POST['termino'] . "', " . $_POST['ativo'] . ")";//ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+*/
+			$sqlAtiCro = "INSERT INTO `cronograma_empresa_plat`(`ID_EMP`, `ATIVIDADES_CROEMPPLA`, `INICIO_CROEMPPLA`, `FIM_CROEMPPLA`, `PARTICIPANTES_CROEMPPLA`, `ATIVO_CROEMPPLA`, `DIAS_CROEMPPLA`) VALUES (1,'". $atividadesSelecionadas . "','" . $_POST['inicio'] . "','" . $_POST['termino'] . "','". $colabSelecionados . "', " . $ativo . ",'" . $diasSelecionados . "')";//ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
 
-		}	
+			echo $sqlAtiCro;
+
+		/*}*/	
 
 		$queryAtiCro = mysql_query($sqlAtiCro, $conexao);
-		header("Location: ../cronograma_empresa.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
+		//header("Location: ../cronograma_empresa.php"); /* REDIRECIONA USUARIO PARA MESMA PAGINA DE SELECAO */
 
 	} else {
 		echo 'Voce nao selecionou nenhuma atividade ';
