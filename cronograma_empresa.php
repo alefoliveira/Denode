398a78f2ce398e8d340092b3c807f813161c31e4
erikacs30 <erika.cs30@gmail.com>
2017-08-13 16:13:26 -0300

    130805

--- cronograma_empresa.php
+++ cronograma_empresa.php
@@ -3,172 +3,188 @@ require 'config.php';
 $conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
 mysql_select_db($banco);
 
-//DADOS SOBRE AS ATIVIDADES LIBERADAS PELA EMPRESA
-$sqlEmp = "SELECT `ATIVIDADES_ATIEMPPLA` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
-$queryEmp = mysql_query($sqlEmp, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$resultEmp = mysql_fetch_array($queryEmp);
-$registros = mysql_num_rows($queryEmp);
-$atividadesEmpresa = explode(",",$resultEmp[0]); //SEPARA ATIVIDADES MARCADAS PELA EMPRESA EM UM ARRAY PARA EXIBIR
-$registrosAtividades =  count($atividadesEmpresa); //CONTA QUANTOS ITENS TEM NO ARRAY
-$contElementos = $registrosAtividades-1; //REMOVE UM ITEM DO CONTADOR CONSIDERANDO QUE O ARRAY TEM O ITEM DE INDICE 0
+session_start();//INICIO SESSAO
+$nome = $_SESSION['NOME_PERFUSU'];
+$idUsu = $_SESSION["ID_PERFUSU"];
+$tipoUsu = $_SESSION["TIPO_PERFUSU"];
 
-//SELECIONA ATIVIDADES PARA FAZER RELAÇÃO COM AQUELAS APROVADAS PELA EMPRESA
-$sql = "SELECT `ID_ATIPLA`, `TITULO_ATIPLA`, `COR_ATIPLA`, `DESCRICAO_ATIPLA`, `DURACAO_ATIPLA`, `MEMBRO_ATIPLA` FROM `ATIVIDADES_PLAT`"; 
-$query = mysql_query($sql, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$result = mysql_fetch_array($query);
+echo "bem vindo ". $nome; //ALTERAR
 
-//DADOS SOBRE OS COLABORADORES
-$sqlColab = "SELECT `ID_PERFUSU`,`CPF_PERFUSU`, `NOME_PERFUSU`, `SOBRENOME_PERFUSU`FROM `perfil_usuario` WHERE `ID_EMP` = 1";
-$queryColab = mysql_query($sqlColab, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$registrosColab = mysql_num_rows($queryColab);
+$sqlUsu = "SELECT `TIPO_PERFUSU` FROM `perfil_usuario` WHERE `ID_PERFUSU`=" . $idUsu; 
+$queryUsu = mysql_query($sqlUsu, $conexao);
+$resultUsu = mysql_fetch_array($queryUsu);
 
-$sqlColab2 = "SELECT `ID_PERFUSU`,`CPF_PERFUSU`, `NOME_PERFUSU`, `SOBRENOME_PERFUSU`FROM `perfil_usuario` WHERE `ID_EMP` = 1";
-$queryColab2 = mysql_query($sqlColab2, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$registrosColab2 = mysql_num_rows($queryColab2);
+if ($resultUsu["TIPO_PERFUSU"] = 2) {
+	echo 'Página não liberada';
+} else {
+
+	//DADOS SOBRE AS ATIVIDADES LIBERADAS PELA EMPRESA
+	$sqlEmp = "SELECT `ATIVIDADES_ATIEMPPLA` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+	$queryEmp = mysql_query($sqlEmp, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+	$resultEmp = mysql_fetch_array($queryEmp);
+	$registros = mysql_num_rows($queryEmp);
+	$atividadesEmpresa = explode(",",$resultEmp[0]); //SEPARA ATIVIDADES MARCADAS PELA EMPRESA EM UM ARRAY PARA EXIBIR
+	$registrosAtividades =  count($atividadesEmpresa); //CONTA QUANTOS ITENS TEM NO ARRAY
+	$contElementos = $registrosAtividades-1; //REMOVE UM ITEM DO CONTADOR CONSIDERANDO QUE O ARRAY TEM O ITEM DE INDICE 0
+
+	//SELECIONA ATIVIDADES PARA FAZER RELAÇÃO COM AQUELAS APROVADAS PELA EMPRESA
+	$sql = "SELECT `ID_ATIPLA`, `TITULO_ATIPLA`, `COR_ATIPLA`, `DESCRICAO_ATIPLA`, `DURACAO_ATIPLA`, `MEMBRO_ATIPLA` FROM `ATIVIDADES_PLAT`"; 
+	$query = mysql_query($sql, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+	$result = mysql_fetch_array($query);
+
+	//DADOS SOBRE OS COLABORADORES
+	$sqlColab = "SELECT `ID_PERFUSU`,`CPF_PERFUSU`, `NOME_PERFUSU`, `SOBRENOME_PERFUSU`FROM `perfil_usuario` WHERE `ID_EMP` = 1";
+	$queryColab = mysql_query($sqlColab, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+	$registrosColab = mysql_num_rows($queryColab);
+
+	$sqlColab2 = "SELECT `ID_PERFUSU`,`CPF_PERFUSU`, `NOME_PERFUSU`, `SOBRENOME_PERFUSU`FROM `perfil_usuario` WHERE `ID_EMP` = 1";
+	$queryColab2 = mysql_query($sqlColab2, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+	$registrosColab2 = mysql_num_rows($queryColab2);
 
 
-//DADOS SESSOES
-$sqlSes = "SELECT `ID_CROEMPPLA`, `ATIVIDADES_CROEMPPLA`, `INICIO_CROEMPPLA`, `FIM_CROEMPPLA`, `PARTICIPANTES_CROEMPPLA`, `ATIVO_CROEMPPLA`, `DIAS_CROEMPPLA` FROM `cronograma_empresa_plat` WHERE `ID_EMP` = 1 ORDER BY `INICIO_CROEMPPLA`";
-$querySes = mysql_query($sqlSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$registrosSes = mysql_num_rows($querySes);
+	//DADOS SESSOES
+	$sqlSes = "SELECT `ID_CROEMPPLA`, `ATIVIDADES_CROEMPPLA`, `INICIO_CROEMPPLA`, `FIM_CROEMPPLA`, `PARTICIPANTES_CROEMPPLA`, `ATIVO_CROEMPPLA`, `DIAS_CROEMPPLA` FROM `cronograma_empresa_plat` WHERE `ID_EMP` = 1 ORDER BY `INICIO_CROEMPPLA`";
+	$querySes = mysql_query($sqlSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+	$registrosSes = mysql_num_rows($querySes);
 
 
 
-?>
+	?>
 
-<!-- _____________________ -->
-<!-- COMEÇO DA PÁGINA HTML -->
-<!-- _____________________ -->
+	<!-- _____________________ -->
+	<!-- COMEÇO DA PÁGINA HTML -->
+	<!-- _____________________ -->
 
-<html lang='pt-br'>
+	<html lang='pt-br'>
 
-	<head>
+		<head>
 
-		<meta charset="utf-8">
+			<meta charset="utf-8">
 
-		<title>ADMIN - MINIMAMENTE</title>
+			<title>ADMIN - MINIMAMENTE</title>
 
-		<!-- ESTILOS -->
-		<link rel="stylesheet" href="CSS/estilo.css">
+			<!-- ESTILOS -->
+			<link rel="stylesheet" href="CSS/estilo.css">
 
-		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+			<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
-		<!-- <script> window.jQuery || document.write('<script src="js/jquery.js"></script>') </script> <!-- Chamada de fallback caso o servidor do jQuery nao carregue -->
-	</head>
+			<!-- <script> window.jQuery || document.write('<script src="js/jquery.js"></script>') </script> <!-- Chamada de fallback caso o servidor do jQuery nao carregue -->
+		</head>
 
-	<body>
+		<body>
 
-		<h1>CRONOGRAMA EMPRESA</h1>
+			<h1>CRONOGRAMA EMPRESA</h1>
 
-		<a href="novo_cronograma_empresa.php">Nova Sessao</a>
+			<a href="novo_cronograma_empresa.php">Nova Sessao</a>
 
-		<table>
+			<table>
 
-				<?php 
-				if ($registrosSes) {
+					<?php 
+					if ($registrosSes) {
 
-					$i = 0;
+						$i = 0;
 
-					while ($resultSes = mysql_fetch_array($querySes)) {
+						while ($resultSes = mysql_fetch_array($querySes)) {
 
-						$atividadesSes = explode(",",$resultSes['ATIVIDADES_CROEMPPLA']); //SEPARA AS ATIVIDADES SELECIONADAS PARA A SESSAO
-						$registrosAtiSes =  count($atividadesSes); //CONTA QUANTOS ITENS TEM NO ARRAY
+							$atividadesSes = explode(",",$resultSes['ATIVIDADES_CROEMPPLA']); //SEPARA AS ATIVIDADES SELECIONADAS PARA A SESSAO
+							$registrosAtiSes =  count($atividadesSes); //CONTA QUANTOS ITENS TEM NO ARRAY
 
-						echo '<tr>';
+							echo '<tr>';
 
-						for ($i=0; $i < $registrosAtiSes; $i++) { //EXIBE NOME DAS ATIVIDADES AO INVES DE ID
-						
-							$sqlAtiSes = 'SELECT `TITULO_ATIPLA` FROM `atividades_plat` WHERE `ID_ATIPLA` =' . $atividadesSes[$i];
-							$queryAtiSes = mysql_query($sqlAtiSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-
-							$totalAtiSes = mysql_result($queryAtiSes, 0, 'TITULO_ATIPLA');
+							for ($i=0; $i < $registrosAtiSes; $i++) { //EXIBE NOME DAS ATIVIDADES AO INVES DE ID
 							
-							if ($i > 0 && $i < $registrosAtiSes) { echo '<td>, ' . $totalAtiSes . ' </td>';} else { echo '<td>' . $totalAtiSes . ' </td>';}
-						}
+								$sqlAtiSes = 'SELECT `TITULO_ATIPLA` FROM `atividades_plat` WHERE `ID_ATIPLA` =' . $atividadesSes[$i];
+								$queryAtiSes = mysql_query($sqlAtiSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
 
-						$participantesSes = explode(",",$resultSes['PARTICIPANTES_CROEMPPLA']); //SEPARA OS PARTICIPANTES SELECIONADOS PARA A SESSAO 
-						$registrosPartSes =  count($participantesSes); 
-
-						for ($iPart=0; $iPart < $registrosPartSes; $iPart++) { //EXIBE NOME DOS PARTICIPANTES AO INVES DO ID
-
-
-							$sqlPartSes = 'SELECT `NOME_PERFUSU`, `SOBRENOME_PERFUSU` FROM `perfil_usuario` WHERE `ID_PERFUSU` =' . $participantesSes[$iPart];
-							$queryPartSes = mysql_query($sqlPartSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-
-							$nomePartSes = mysql_result($queryPartSes, 0, 'NOME_PERFUSU');
-							
-							if ($iPart> 0 && $iPart < $registrosPartSes) {
-								echo '<td>, ' . $nomePartSes . '</td>';
-							} else {
-								echo '<td>' . $nomePartSes . '</td>';
+								$totalAtiSes = mysql_result($queryAtiSes, 0, 'TITULO_ATIPLA');
+								
+								if ($i > 0 && $i < $registrosAtiSes) { echo '<td>, ' . $totalAtiSes . ' </td>';} else { echo '<td>' . $totalAtiSes . ' </td>';}
 							}
-						}
 
-						$diasSes = explode(",",$resultSes['DIAS_CROEMPPLA']); //SEPARA OS DIAS SELECIONADOS PARA A SESSAO 
-						if (in_array(1, $diasSes)){ echo '<td>Segunda </td>'; }
-						if (in_array(2, $diasSes)){ echo '<td>Terça </td>'; }
-						if (in_array(3, $diasSes)){ echo '<td>Quarta </td>'; }
-						if (in_array(4, $diasSes)){ echo '<td>Quinta </td>'; }
-						if (in_array(5, $diasSes)){ echo '<td>Sexta</td>'; }
+							$participantesSes = explode(",",$resultSes['PARTICIPANTES_CROEMPPLA']); //SEPARA OS PARTICIPANTES SELECIONADOS PARA A SESSAO 
+							$registrosPartSes =  count($participantesSes); 
 
-						if (in_array($localtime[6], $diasSes)){ //VERIFICA SE A SESSAO ACONTECERA NO DIA DE HOJE
-
-							$inicioSes = explode(":",$resultSes['INICIO_CROEMPPLA']); //SEPARA A HORA DOS MINUTOS DO INICIO DA SESSAO
-							
-							
+							for ($iPart=0; $iPart < $registrosPartSes; $iPart++) { //EXIBE NOME DOS PARTICIPANTES AO INVES DO ID
 
 
-							if ($localtime[2] == $inicioSes[0] || $localtime[2] == $inicioSes[0]-1){ //VERIFICA SE O HORARIO ATUAL EH IGUAL AO HORARIO DA SESSAO OU SE EH 1H ANTES
-								echo "\n sessao = " . ($inicioSes[0]);
-								echo "\n localtime = " . $localtime[2];
+								$sqlPartSes = 'SELECT `NOME_PERFUSU`, `SOBRENOME_PERFUSU` FROM `perfil_usuario` WHERE `ID_PERFUSU` =' . $participantesSes[$iPart];
+								$queryPartSes = mysql_query($sqlPartSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
 
-								if ($localtime[1] == 45 || $inicioSes[1]-15 == $localtime[1]) {	//VERIFICA SE FALTAM 15 MINUTOS PARA A SESSAO COMECAR
-
-									echo '<a href="confirma_sessao.php?ID_USU=1&ID_SESSAO='. $resultSes['ID_CROEMPPLA'] .'" target="blank">
-										CONFIRMAR PARTICIPACAO
-									</a>';
-								}  else if ($localtime[1] == 55 || $inicioSes[1]-5 == $localtime[1]) { 
-
-									$sqlConfSes = "SELECT `CONFIRMADOS_SESPLA` FROM `sessao_plat` WHERE `ID_CROEMPPLA` = ". $resultSes['ID_CROEMPPLA']; //SELECIONA TODOS OS COLABORADORES QUE CONFIRMARAM PRESENCA NA SESSAO
-									$queryConfSes = mysql_query($sqlConfSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-									$resultConfSes = mysql_fetch_array($queryConfSes);
-
-									$confirmadosSes = explode(",",$resultConfSes['CONFIRMADOS_SESPLA']); //COLOCA OS CONFIRMADOS EM UM ARRAY
-									
-									$adminSes = array_rand($confirmadosSes, 1); //SORTEIA UM INDICE ENTRE OS POSSIVEIS NO ARRAY DE CONFIRMADOS
-
-									echo ' admin =' . $confirmadosSes[$adminSes]; //SELECIONA O CONFIRMADO SORTEADO NA VARIAVEL $adminSes
-
-									echo '<a href="sessao.php?ID_SESSAO='. $resultSes['ID_CROEMPPLA'] .'" target="blank">
-										ACESSAR SESSAO
-									</a>'; //ENVIA LINK DA SESSAO PARA ADMIN
+								$nomePartSes = mysql_result($queryPartSes, 0, 'NOME_PERFUSU');
+								
+								if ($iPart> 0 && $iPart < $registrosPartSes) {
+									echo '<td>, ' . $nomePartSes . '</td>';
+								} else {
+									echo '<td>' . $nomePartSes . '</td>';
 								}
-
-							} else {
-								echo '<td>LOCALTIME PORRAAA </td>';
 							}
+
+							$diasSes = explode(",",$resultSes['DIAS_CROEMPPLA']); //SEPARA OS DIAS SELECIONADOS PARA A SESSAO 
+							if (in_array(1, $diasSes)){ echo '<td>Segunda </td>'; }
+							if (in_array(2, $diasSes)){ echo '<td>Terça </td>'; }
+							if (in_array(3, $diasSes)){ echo '<td>Quarta </td>'; }
+							if (in_array(4, $diasSes)){ echo '<td>Quinta </td>'; }
+							if (in_array(5, $diasSes)){ echo '<td>Sexta</td>'; }
+
+							if (in_array($localtime[6], $diasSes)){ //VERIFICA SE A SESSAO ACONTECERA NO DIA DE HOJE
+
+								$inicioSes = explode(":",$resultSes['INICIO_CROEMPPLA']); //SEPARA A HORA DOS MINUTOS DO INICIO DA SESSAO
+								
+								
+
+
+								if ($localtime[2] == $inicioSes[0] || $localtime[2] == $inicioSes[0]-1){ //VERIFICA SE O HORARIO ATUAL EH IGUAL AO HORARIO DA SESSAO OU SE EH 1H ANTES
+									echo "\n sessao = " . ($inicioSes[0]);
+									echo "\n localtime = " . $localtime[2];
+
+									if ($localtime[1] == 45 || $inicioSes[1]-15 == $localtime[1]) {	//VERIFICA SE FALTAM 15 MINUTOS PARA A SESSAO COMECAR
+
+										echo '<a href="confirma_sessao.php?ID_USU=1&ID_SESSAO='. $resultSes['ID_CROEMPPLA'] .'" target="blank">
+											CONFIRMAR PARTICIPACAO
+										</a>';
+									}  else if ($localtime[1] == 55 || $inicioSes[1]-5 == $localtime[1]) { 
+
+										$sqlConfSes = "SELECT `CONFIRMADOS_SESPLA` FROM `sessao_plat` WHERE `ID_CROEMPPLA` = ". $resultSes['ID_CROEMPPLA']; //SELECIONA TODOS OS COLABORADORES QUE CONFIRMARAM PRESENCA NA SESSAO
+										$queryConfSes = mysql_query($sqlConfSes, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+										$resultConfSes = mysql_fetch_array($queryConfSes);
+
+										$confirmadosSes = explode(",",$resultConfSes['CONFIRMADOS_SESPLA']); //COLOCA OS CONFIRMADOS EM UM ARRAY
+										
+										$adminSes = array_rand($confirmadosSes, 1); //SORTEIA UM INDICE ENTRE OS POSSIVEIS NO ARRAY DE CONFIRMADOS
+
+										echo ' admin =' . $confirmadosSes[$adminSes]; //SELECIONA O CONFIRMADO SORTEADO NA VARIAVEL $adminSes
+
+										echo '<a href="sessao.php?ID_SESSAO='. $resultSes['ID_CROEMPPLA'] .'" target="blank">
+											ACESSAR SESSAO
+										</a>'; //ENVIA LINK DA SESSAO PARA ADMIN
+									}
+
+								} else {
+									echo '<td>LOCALTIME PORRAAA </td>';
+								}
+							}
+
+							echo '
+							<td>  ' . $resultSes['INICIO_CROEMPPLA'] . ' </td> 
+							<td>' . $resultSes['FIM_CROEMPPLA'] . '</td>
+							<td>' . $resultSes['ATIVO_CROEMPPLA'] . '</td>
+							<td> 
+								<a href="apoio/editar_cronograma_empresa.php?ID_CROEMPPLA='.$resultSes['ID_CROEMPPLA'].'" target="blank">
+									Editar
+								</a>
+							</td>
+							<td id="icon-center"> 
+								<a href="apoio/excluir_cronograma_empresa.php?ID_CROEMPPLA='.$resultSes['ID_CROEMPPLA'].'">
+									Excluir
+								</a>
+							</td>
+							</tr>';
+
+								
+
 						}
+					} ?>
+		</body>
 
-						echo '
-						<td>  ' . $resultSes['INICIO_CROEMPPLA'] . ' </td> 
-						<td>' . $resultSes['FIM_CROEMPPLA'] . '</td>
-						<td>' . $resultSes['ATIVO_CROEMPPLA'] . '</td>
-						<td> 
-							<a href="apoio/editar_cronograma_empresa.php?ID_CROEMPPLA='.$resultSes['ID_CROEMPPLA'].'" target="blank">
-								Editar
-							</a>
-						</td>
-						<td id="icon-center"> 
-							<a href="apoio/excluir_cronograma_empresa.php?ID_CROEMPPLA='.$resultSes['ID_CROEMPPLA'].'">
-								Excluir
-							</a>
-						</td>
-						</tr>';
-
-							
-
-					}
-				} ?>
-	</body>
-
-</html>
+	</html>
+<?php } ?>
--- selecao_atividades.php
+++ selecao_atividades.php
@@ -1,113 +1,129 @@
 <?php
 require 'config.php';
 
-$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
-mysql_select_db($banco);
+	$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
+	mysql_select_db($banco);
+
+	session_start();//INICIO SESSAO
+	$nome = $_SESSION['NOME_PERFUSU'];
+	$idUsu = $_SESSION["ID_PERFUSU"];
+	$tipoUsu = $_SESSION["TIPO_PERFUSU"];
+	
+	echo "bem vindo ". $nome; //ALTERAR
+
+	$sqlUsu = "SELECT `TIPO_PERFUSU` FROM `perfil_usuario` WHERE `ID_PERFUSU`=" . $idUsu; 
+	$queryUsu = mysql_query($sqlUsu, $conexao);
+	$resultUsu = mysql_fetch_array($queryUsu);
+
+	if ($resultUsu["TIPO_PERFUSU"] = 2) {
+		echo 'Página não liberada';
+	} else {
 
 
-$sql = "SELECT `ID_ATIPLA`, `TITULO_ATIPLA`, `COR_ATIPLA`, `DESCRICAO_ATIPLA`, `DURACAO_ATIPLA` FROM `ATIVIDADES_PLAT` ORDER BY `ID_ATIPLA`"; 
-$query = mysql_query($sql, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$registros = mysql_num_rows($query); //CONTADOR DE RESULTADOS TRAZIDOS DO BANCO DE DADOS
+		$sql = "SELECT `ID_ATIPLA`, `TITULO_ATIPLA`, `COR_ATIPLA`, `DESCRICAO_ATIPLA`, `DURACAO_ATIPLA` FROM `ATIVIDADES_PLAT` ORDER BY `ID_ATIPLA`"; 
+		$query = mysql_query($sql, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+		$registros = mysql_num_rows($query); //CONTADOR DE RESULTADOS TRAZIDOS DO BANCO DE DADOS
 
-$sqlEmp = "SELECT `ATIVIDADES_ATIEMPPLA`, `PONTOS_ATIEMPPLA` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
+		$sqlEmp = "SELECT `ATIVIDADES_ATIEMPPLA`, `PONTOS_ATIEMPPLA` from ATIVIDADES_EMPRESA_PLAT WHERE ID_EMP = 1"; //ALTERAR PARA COLOCAR ID DA EMPRESA TRAZIDO NA SESSION
 
-$queryEmp = mysql_query($sqlEmp, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-$resultEmp = mysql_fetch_array($queryEmp);
+		$queryEmp = mysql_query($sqlEmp, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
+		$resultEmp = mysql_fetch_array($queryEmp);
 
-$atividadesEmpresa = explode(",",$resultEmp[0]); //SEPARA ATIVIDADES MARCADAS PELA EMPRESA EM UM ARRAY PARA MARCAR OS CHECKBOXES
-$registrosAtividades =  count($atividadesEmpresa); //CONTA QUANTOS ITENS TEM NO ARRAY
-$contElementos = $registrosAtividades-1; //REMOVE UM ITEM DO CONTADOR CONSIDERANDO QUE O ARRAY TEM O ITEM DE INDICE 0
+		$atividadesEmpresa = explode(",",$resultEmp[0]); //SEPARA ATIVIDADES MARCADAS PELA EMPRESA EM UM ARRAY PARA MARCAR OS CHECKBOXES
+		$registrosAtividades =  count($atividadesEmpresa); //CONTA QUANTOS ITENS TEM NO ARRAY
+		$contElementos = $registrosAtividades-1; //REMOVE UM ITEM DO CONTADOR CONSIDERANDO QUE O ARRAY TEM O ITEM DE INDICE 0
 
-?>
+		?>
 
-<!-- _____________________ -->
-<!-- COMEÇO DA PÁGINA HTML -->
-<!-- _____________________ -->
+		<!-- _____________________ -->
+		<!-- COMEÇO DA PÁGINA HTML -->
+		<!-- _____________________ -->
 
-<html lang='pt-br'>
+		<html lang='pt-br'>
 
-	<head>
+			<head>
 
-		<meta charset="utf-8">
+				<meta charset="utf-8">
 
-		<title>ADMIN - MINIMAMENTE</title>
+				<title>ADMIN - MINIMAMENTE</title>
 
-		<!-- ESTILOS -->
-		<link rel="stylesheet" href="CSS/estilo.css">
-		<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+				<!-- ESTILOS -->
+				<link rel="stylesheet" href="CSS/estilo.css">
+				<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
-	</head>
+			</head>
 
-	<body>
+			<body>
 
-		<h1>ATIVIDADES DISPONÍVEIS</h1>
+				<h1>ATIVIDADES DISPONÍVEIS</h1>
 
-		<form name="selecao_atividades" action="apoio\insere_atividades_selecionadas.php" method="post" enctype="multipart/form-data">
+				<form name="selecao_atividades" action="apoio\insere_atividades_selecionadas.php" method="post" enctype="multipart/form-data">
 
-			<input type="hidden" name="cadastrar" value="1" />
-			
-			<table>
-				<?php
-				if ($registros) {
+					<input type="hidden" name="cadastrar" value="1" />
+					
+					<table>
+						<?php
+						if ($registros) {
 
-					$i = 0;
+							$i = 0;
 
-					while ($result = mysql_fetch_array($query)) {
+							while ($result = mysql_fetch_array($query)) {
 
-						echo '<tr>
-							<!--td>  ' . $result['ID_ATIPLA'] . ' </td --> 
-							<td>  ' . $result['TITULO_ATIPLA'] . ' </td> 
-							<td>' . $result['DESCRICAO_ATIPLA'] . '</td>
-							<td>  ' . $result['DURACAO_ATIPLA'] . ' </td>
-							<td>';
-						
-						if ($i <= $contElementos) {
-							if ($result['ID_ATIPLA'] == $atividadesEmpresa[$i]) {
+								echo '<tr>
+									<!--td>  ' . $result['ID_ATIPLA'] . ' </td --> 
+									<td>  ' . $result['TITULO_ATIPLA'] . ' </td> 
+									<td>' . $result['DESCRICAO_ATIPLA'] . '</td>
+									<td>  ' . $result['DURACAO_ATIPLA'] . ' </td>
+									<td>';
 								
-								echo ' <input type="checkbox" class="checkbox" name="atividades[]" checked id="' . $result['ID_ATIPLA'] . '" value="' . $result['ID_ATIPLA'] . '">
-									</td>
-									<td><input type="text" class="pontosAti" name="pontos[]" id="input' . $result['ID_ATIPLA'] . '" placeholder="Pontos" /></td>
-								</tr>';
-								$i++;
+								if ($i <= $contElementos) {
+									if ($result['ID_ATIPLA'] == $atividadesEmpresa[$i]) {
+										
+										echo ' <input type="checkbox" class="checkbox" name="atividades[]" checked id="' . $result['ID_ATIPLA'] . '" value="' . $result['ID_ATIPLA'] . '">
+											</td>
+											<td><input type="text" class="pontosAti" name="pontos[]" id="input' . $result['ID_ATIPLA'] . '" placeholder="Pontos" /></td>
+										</tr>';
+										$i++;
 
-							} else {
-								echo ' <input type="checkbox" class="checkbox" name="atividades[]" id="' . $result['ID_ATIPLA'] . '" value="' . $result['ID_ATIPLA'] . '">
-									</td>
-									<td><input type="text" class="pontosAti" name="pontos[]" id="input' . $result['ID_ATIPLA'] . '" placeholder="Pontos" disabled /></td>
-								</tr>';
+									} else {
+										echo ' <input type="checkbox" class="checkbox" name="atividades[]" id="' . $result['ID_ATIPLA'] . '" value="' . $result['ID_ATIPLA'] . '">
+											</td>
+											<td><input type="text" class="pontosAti" name="pontos[]" id="input' . $result['ID_ATIPLA'] . '" placeholder="Pontos" disabled /></td>
+										</tr>';
+									}
+								} else {
+									echo ' <input type="checkbox" class="checkbox" name="atividades[]" id="' . $result['ID_ATIPLA'] . '" value="' . $result['ID_ATIPLA'] . '">
+										</td>
+										<td><input type="text" class="pontosAti" name="pontos[]" id="input' . $result['ID_ATIPLA'] . '" placeholder="Pontos" disabled /></td>
+									</tr>';
+									$i++;
+								}
+
 							}
 						} else {
-							echo ' <input type="checkbox" class="checkbox" name="atividades[]" id="' . $result['ID_ATIPLA'] . '" value="' . $result['ID_ATIPLA'] . '">
-								</td>
-								<td><input type="text" class="pontosAti" name="pontos[]" id="input' . $result['ID_ATIPLA'] . '" placeholder="Pontos" disabled /></td>
-							</tr>';
-							$i++;
+							//ALTERAR
+							echo '<p>Nenhuma atividade disponivel no momento!</p>';
+						} ?>
+					</table>
+
+					<input type="submit" value="Salvar Atividades">
+				</form>
+
+			</body>
+
+
+			<script>
+				$(document).ready(function(){
+					$('.checkbox').click(function(){
+						var idCheckbox = parseInt($(this).attr('id')); //PEGA O ID DO CHECKBOX CLICADO E TRANSFORMA EM NÚMERO (INT)
+						if ($(this).is(':checked')){
+							$('#input' + idCheckbox).removeAttr("disabled");
+						} else {
+							$('#input' + idCheckbox).attr("disabled","disabled");
 						}
+					});
+				});
+			</script>
 
-					}
-				} else {
-					//ALTERAR
-					echo '<p>Nenhuma atividade disponivel no momento!</p>';
-				} ?>
-			</table>
-
-			<input type="submit" value="Salvar Atividades">
-		</form>
-
-	</body>
-
-
-	<script>
-		$(document).ready(function(){
-			$('.checkbox').click(function(){
-				var idCheckbox = parseInt($(this).attr('id')); //PEGA O ID DO CHECKBOX CLICADO E TRANSFORMA EM NÚMERO (INT)
-				if ($(this).is(':checked')){
-					$('#input' + idCheckbox).removeAttr("disabled");
-				} else {
-					$('#input' + idCheckbox).attr("disabled","disabled");
-				}
-			});
-		});
-	</script>
-
-</html>
+		</html>
+	<?php } ?>
