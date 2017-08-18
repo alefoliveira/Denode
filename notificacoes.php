6eeed9c75ce83e36445236f2899f48c2c7960660
erikacs30 <erika.cs30@gmail.com>
2017-08-18 15:37:43 -0300

    180801

--- notificacoes.php
+++ notificacoes.php
@@ -14,7 +14,7 @@ $queryUsu = mysql_query($sqlUsu, $conexao);
 $resultUsu = mysql_fetch_array($queryUsu);
 
 
-$sqlNot = "SELECT `DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 0"; //SELECIONA TODAS AS NOTIFICACOES NAO VISUALIZADAS
+$sqlNot = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 0"; //SELECIONA TODAS AS NOTIFICACOES NAO VISUALIZADAS
 $queryNot = mysql_query($sqlNot, $conexao);
 
 $qtdNot = 0;
@@ -24,10 +24,9 @@ while ($resultNot = mysql_fetch_array($queryNot)){
 }
 
 
-$sqlNotVis = "SELECT `DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 1"; //SELECIONA TODAS AS NOTIFICACOES VISUALIZADAS
-$queryNotVis = mysql_query($sqlNotVis, $conexao);
-$resultNotVis = mysql_fetch_array($queryNotVis);
 
+$sqlNotVis = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 1"; //SELECIONA TODAS AS NOTIFICACOES VISUALIZADAS
+$queryNotVis = mysql_query($sqlNotVis, $conexao);
 ?>
 
 <!-- _____________________ -->
@@ -58,6 +57,71 @@ $resultNotVis = mysql_fetch_array($queryNotVis);
 	<?php echo $idUsu ?>
 
 	<div id="contadorNot">Qtd Notif.: <?php echo $qtdNot ?></div>
+
+	<div id="notificacoes">
+	oi
+		<?php 
+
+		echo 'boi';
+
+		$sqlNot2 = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 0"; //SELECIONA TODAS AS NOTIFICACOES NAO VISUALIZADAS
+		$queryNot2 = mysql_query($sqlNot2, $conexao);
+		while ($resultNot2 = mysql_fetch_array($queryNot2)){
+			
+			echo '<li style="color:'.$resultNot2['COR_NOTPLA'].'" class="novaNot" id="'.$resultNot2['ID_NOTPLA'].'">'.$resultNot2['DESCRICAO_NOTPLA'].'<br/>'.$resultNot2['DATA_NOTPLA'].'</li>';
+
+
+			echo'
+				<script>
+					$("#'.$resultNot2['ID_NOTPLA'].'").click(function(e){
+		 				';
+		 				$sqlNotUpd = "UPDATE `notificacoes_plat` SET `STATUS_NOTPLA`= 1 WHERE `ID_NOTPLA` =" . $resultNot2['ID_NOTPLA']; //SELECIONA TODAS AS NOTIFICACOES VISUALIZADAS
+						$queryNotUpd = mysql_query($sqlNotUpd, $conexao);
+						$qtdNot -= 1;
+
+					echo '
+						console.log("atualizou");
+					});
+				</script>
+			';
+		} 
+
+		$sqlNotVis2 = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 1"; //SELECIONA TODAS AS NOTIFICACOES VISUALIZADAS
+		$queryNotVis2 = mysql_query($sqlNotVis2, $conexao);
+
+		 while ($resultNotVis2 = mysql_fetch_array($queryNotVis2)){
+			echo '<li id="'.$resultNotVis2['ID_NOTPLA'].'">'.$resultNotVis2['DESCRICAO_NOTPLA'].'<br/>'.$resultNotVis2['DATA_NOTPLA'].'</li>';
+
+		} ?>
+	</div>
+
+	<script type="text/javascript">
+		
+		$(document).ready(function(){
+
+
+
+			$("#notificacoes").css('display', 'none');
+
+
+			$("#contadorNot").click(function(e){
+				e.stopPropagation();
+ 				$("#notificacoes").css('display', 'block');
+			});
+
+			/*$(document).click(function(e){
+				$("#notificacoes").css('display', 'none');
+			});*/
+
+			$("#contadorNot").click(function(e){
+				e.stopPropagation();
+ 				$("#notificacoes").css('display', 'block');
+			});
+
+		});
+
+
+	</script>
 
 </body>
 
