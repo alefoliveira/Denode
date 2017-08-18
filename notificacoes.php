12ef9ac05073ca48a47b9610fa56061b2c226ada
erikacs30 <erika.cs30@gmail.com>
2017-08-17 21:33:31 -0300

    170801

---
+++ notificacoes.php
@@ -0,0 +1,64 @@
+<?php
+require 'config.php';
+$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
+mysql_select_db($banco);
+
+session_start();//INICIO SESSAO
+$nome = $_SESSION['NOME_PERFUSU'];
+$idUsu = $_SESSION["ID_PERFUSU"];
+
+echo "bem vindo ". $nome; //ALTERAR
+
+$sqlUsu = "SELECT `TIPO_PERFUSU`, `ID_EMP` FROM `perfil_usuario` WHERE `ID_PERFUSU`=" . $idUsu; 
+$queryUsu = mysql_query($sqlUsu, $conexao);
+$resultUsu = mysql_fetch_array($queryUsu);
+
+
+$sqlNot = "SELECT `DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 0"; //SELECIONA TODAS AS NOTIFICACOES NAO VISUALIZADAS
+$queryNot = mysql_query($sqlNot, $conexao);
+
+$qtdNot = 0;
+
+while ($resultNot = mysql_fetch_array($queryNot)){
+	$qtdNot += 1; //CONTA QUANTAS NOTIFICACOES NAO VISUALIZADAS O USUARIO TEM
+}
+
+
+$sqlNotVis = "SELECT `DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 1"; //SELECIONA TODAS AS NOTIFICACOES VISUALIZADAS
+$queryNotVis = mysql_query($sqlNotVis, $conexao);
+$resultNotVis = mysql_fetch_array($queryNotVis);
+
+?>
+
+<!-- _____________________ -->
+<!-- COMEÇO DA PÁGINA HTML -->
+<!-- _____________________ -->
+
+<html lang='pt-br'>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>ADMIN - MINIMAMENTE</title>
+
+	<!-- ESTILOS -->
+	<link rel="stylesheet" href="CSS/estilo.css">
+
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+
+	<!-- <script> window.jQuery || document.write('<script src="js/jquery.js"></script>') </script> <!-- Chamada de fallback caso o servidor do jQuery nao carregue -->
+</head>
+
+<body>
+
+	<h1>Notificacoes</h1>
+
+	<a href="historicoNotificacoes.php?idUsu=">Ver Mais</a>
+	<?php echo $idUsu ?>
+
+	<div id="contadorNot">Qtd Notif.: <?php echo $qtdNot ?></div>
+
+</body>
+
+</html>
