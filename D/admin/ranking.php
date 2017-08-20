8bbb44c6851d7f375d2a9b3f36d29f0b73be8984
i-ianka <inglidianka@gmail.com>
2017-08-20 20:33:28 -0300

    Update ranking.php

--- D/admin/ranking.php
+++ D/admin/ranking.php
@@ -1,66 +1,8 @@
 <?php
-
 require 'config.php';
-$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
-mysql_select_db($banco);
-
-
-		function redirect(){
-			$flag=0;
-			$conexao=$this->banco();
-			$sql="Select ID_PERFUSU from perfil_usuario";
-			$result = $conexao->query($sql);
-
-		
-			if($flag==0)
-				header("login.php");
-
-			$conexao->close();
-		}
-
-		function redirectlogin(){
-			$flag=0;
-			$conexao=$this->banco();
-			$sql="Select ID_PERFUSU from perfil_usuario";
-			$result = $conexao->query($sql);
-
-			if($_SESSION["NOME_PERFUSU"] == "admin")
-				$flag=1;
-
-			if($result->num_rows >0)
-				while($row = $result->fetch_assoc())
-					if($_SESSION["NOME_PERFUSU"] == $row["ID_PERFUSU"]){
-						$flag=2;
-						break;
-					}
-
-			else if($flag==1)
-				header("Location: perfiladmin.php");
-			else if($flag==2)
-				header("Location:usuarios.php");
-
-			$conexao->close();
-		
-	} 
+  $conexao = @mysqli_connect($host, $usuario, $senha, $banco) or Die();
 ?>
-<?php	
-		$sql = "SELECT ID_PERFUSU FROM perfil_usuario";
-	    $query = mysql_query($sql, $conexao); //ESTABELECE CONEXAO ENTRE QUERY ($sql) E O BANCO DE DADOS
-        $registros = mysql_num_rows($query); //CONTADOR DE RESULTADOS TRAZIDOS DO BANCO DE DADOS
-		//Inserting values in dropdown
-		echo "<select name='STD'>";
-		echo "<option value='idusuario'>Id Usuário</option>";
 
-		if ($query->num_rows > 0)
-			while ($row = $result->fetch_assoc())
-				echo "<option value='" . $row['ID_PERFUSU'] . "'>" . $row['ID_PERFUSU'] . "</option>";
-		else
-			echo "0 results";
-		echo "</select>";
-		
-
-
-?>
 <!DOCTYPE html>
 	<head>
 		<title>Gerenciar usuário</title>
@@ -75,22 +17,18 @@ mysql_select_db($banco);
 				<div class=output>
 					<?php
 						
-						$conexao = @mysql_connect($host, $usuario, $senha) or exit(mysql_error());
-                        mysql_select_db($banco);
-
 						$sql="SELECT * FROM perfil_usuario order by PONTOS_PERFUSU DESC";
-							 $query = mysql_query($sql, $conexao);
-                           $registros = mysql_num_rows($query); //CONTADOR DE RESULTADOS TRAZIDOS DO BANCO DE DADOS
+							$result = $conexao->query($sql);
                               $i= 1; 
                             
-						if($query->num_rows > 0 || $i){
+						if($result->num_rows > 0 || $i){
 							echo "<table class=slist>";
 							echo "<tr>";
 							echo "<th>Colocação &nbsp; &nbsp;</td> ";
 						    echo "<th>Usuário</td> ";
 							echo "<th>Pontuação</td>";
 							echo "</tr>";
-							while($row = $query->fetch_assoc()){
+							while($row = $result->fetch_assoc()){
 								echo "<tr>";
 								echo 	"<td>" . $i++ . "° &nbsp; &nbsp;&nbsp; &nbsp;</td>";
 								echo 	"<td>" . $row["NOME_PERFUSU"] . "</td>";
@@ -99,11 +37,11 @@ mysql_select_db($banco);
 							}	
 						}
 						else
-							echo "<div align='center' style='font-size:20px'>No Records.</div>";
+							echo "<div align='center' style='font-size:20px'>Naõ há dados.</div>";
 
 						
 
-						echo "</table>";
+						echo "</table>";  
 
 						$conexao->close();
 					?>
