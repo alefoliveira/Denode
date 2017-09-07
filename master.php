7f1b3bcaba8d7c022c5640f8dde8a7d59efc1e60
erikacs30 <erika.cs30@gmail.com>
2017-09-07 17:07:45 -0300

    070902

--- master.php
+++ master.php
@@ -3,78 +3,102 @@
 		<link rel="stylesheet" type="text/css" href="css/master.css">
 	</head>
 	<body>
-		<section id="menu_lateral">
-			<img src="img/logo_denode.svg" id="logo" />
-			<ul id="menu_colab">
-				<li>
-					<a href="">
-						<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
-						<p>Início</p>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-						<p>Agenda</p>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-						<p>Pontos</p>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-						<p>Conquistas</p>
-					</a>
-				</li>
-				<li>
-					<a href="">
-						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-						<p>Relatório</p>
-					</a>
-				</li>
-			</ul>
-			<ul id="menu_admin">
-				<li>
-					<img src="img/iconset.svg" />
-					<p>Início</p>
-				</li>
-				<li>
-					<img src="img/iconset.svg" />
-					<p>Agenda</p>
-				</li>
-				<li>
-					<img src="img/iconset.svg" />
-					<p>Equipe</p>
-				</li>
-				<li>
-					<img src="img/iconset.svg" />
-					<p>Pontuação</p>
-				</li>
-				<li>
-					<img src="img/iconset.svg" />
-					<p>Conquistas/p>
+		<section id="navegacao">
+			<section id="menu_lateral">
+				<img src="img/logo_denode.svg" id="logo" />
+				<ul id="menu_colab">
 					<li>
-					<img src="img/iconset.svg" />
-					<p>Comunicados</p>
-				</li>
-				<li>
-					<img src="img/iconset.svg" />
-					<p>Relatórios</p>
-				</li>
-			</ul>
-		</section>
-		<section id="menu_superior">
-			<ul>
-				<li>Ergonomia</li>
-				<li>
-					<img src="img/iconset.svg" />
-					<img src="img/iconset.svg" />
-				</li>
-			</ul>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
+							<p>Início</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+							<p>Agenda</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+							<p>Pontos</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+							<p>Conquistas</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+							<p>Relatório</p>
+						</a>
+					</li>
+				</ul>
+				<ul id="menu_admin">
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
+							<p>Início</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+							<p>Agenda</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+							<p>Equipe</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+							<p>Pontuação</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+							<p>Conquistas</p>
+						</a>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+							<p>Comunicados</p>
+						</a>
+					</li>
+					<li>
+						<a href="">
+							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+							<p>Relatórios</p>
+						</a>
+					</li>
+				</ul>
+			</section>
+			<section id="menu_superior">
+				<ul>
+					<li id="botao">
+						<a href="">Ergonomia</a>
+					</li>
+					<li>
+						<img src="img/iconset.svg#svgView(viewBox(4, 115, 18, 23))" alt="Agenda">
+					</li>
+					<li id="menu_subitens">
+						<p>Ajuda</p>
+						<img src="img/iconset.svg#svgView(viewBox(7, 134, 11, 29))" alt="Agenda">
+					</li>
+					<li>
+						<p>Olá, </p>
+					</li>
+				</ul>
+			</section>	
 		</section>
 	</body>
 </html>
