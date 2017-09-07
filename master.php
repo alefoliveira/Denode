2e91dc26459291f6d8f52694112afdbd6439d6f0
erikacs30 <erika.cs30@gmail.com>
2017-09-07 13:19:06 -0300

    060901

---
+++ master.php
@@ -0,0 +1,80 @@
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="css/master.css">
+	</head>
+	<body>
+		<section id="menu_lateral">
+			<img src="img/logo_denode.svg" />
+			<ul id="menu_colab">
+				<li>
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
+						<p>Início</p>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 25, 23, 23))" alt="Agenda">
+						<p>Agenda</p>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 51, 23, 23))" alt="Agenda">
+						<p>Pontos</p>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 25, 23, 23))" alt="Agenda">
+						<p>Conquistas</p>
+					</a>
+				</li>
+				<li>
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 25, 23, 23))" alt="Agenda">
+						<p>Relatório</p>
+					</a>
+				</li>
+			</ul>
+			<ul id="menu_admin">
+				<li>
+					<img src="img/iconset.svg" />
+					<p>Início</p>
+				</li>
+				<li>
+					<img src="img/iconset.svg" />
+					<p>Agenda</p>
+				</li>
+				<li>
+					<img src="img/iconset.svg" />
+					<p>Equipe</p>
+				</li>
+				<li>
+					<img src="img/iconset.svg" />
+					<p>Pontuação</p>
+				</li>
+				<li>
+					<img src="img/iconset.svg" />
+					<p>Conquistas/p>
+					<li>
+					<img src="img/iconset.svg" />
+					<p>Comunicados</p>
+				</li>
+				<li>
+					<img src="img/iconset.svg" />
+					<p>Relatórios</p>
+				</li>
+			</ul>
+		</section>
+		<section id="menu_superior">
+			<ul>
+				<li>Ergonomia</li>
+				<li>
+					<img src="img/iconset.svg" />
+					<img src="img/iconset.svg" />
+				</li>
+			</ul>
+		</section>
+	</body>
+</html>
