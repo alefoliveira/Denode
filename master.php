a78631aba889e89e5c9452ff0fe89642a171db9e
erikacs30 <erika.cs30@gmail.com>
2017-09-07 19:13:07 -0300

    070904

--- master.php
+++ master.php
@@ -1,105 +1,127 @@
-<html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="css/master.css">
-	</head>
-	<body>
-		<section id="navegacao">
-			<section id="menu_lateral">
-				<img src="img/logo_denode.svg" id="logo" />
-				<ul id="menu_colab">
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
-							<p>Início</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-							<p>Agenda</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-							<p>Pontos</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-							<p>Conquistas</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-							<p>Relatório</p>
-						</a>
-					</li>
-				</ul>
-				<ul id="menu_admin">
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
-							<p>Início</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
-							<p>Agenda</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-							<p>Equipe</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-							<p>Pontuação</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-							<p>Conquistas</p>
-						</a>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-							<p>Comunicados</p>
-						</a>
-					</li>
-					<li>
-						<a href="">
-							<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
-							<p>Relatórios</p>
-						</a>
-					</li>
-				</ul>
-			</section>
-			<section id="menu_superior">
-				<ul>
-					<li id="botao">
-						<a href="">Ergonomia</a>
-					</li>
-					<li id="notificacoes">
-						<img src="img/iconset.svg#svgView(viewBox(4, 115, 18, 23))" alt="Agenda">
-						<p id="not_cont">12</p>
-					</li>
-					<li id="menu_subitens">
-						<p>Ajuda</p>
-						<img src="img/iconset.svg#svgView(viewBox(7, 134, 11, 29))" alt="Agenda">
-					</li>
-					<li>
-						<p>Olá, </p>
-					</li>
-				</ul>
-			</section>	
-		</section>
-	</body>
-</html>
+<?php
+
+$sqlNot = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 0"; //SELECIONA TODAS AS NOTIFICACOES NAO VISUALIZADAS
+$queryNot = mysql_query($sqlNot, $conexao);
+
+$qtdNot = 0;
+
+while ($resultNot = mysql_fetch_array($queryNot)){
+	$qtdNot += 1; //CONTA QUANTAS NOTIFICACOES NAO VISUALIZADAS O USUARIO TEM
+}
+
+?>
+
+<section id="navegacao">
+	<section id="menu_lateral">
+		<img src="img/logo_denode.svg" id="logo" />
+
+		<?php
+
+			$sqlUsu = "SELECT `TIPO_PERFUSU`, `ID_EMP` FROM `perfil_usuario` WHERE `ID_PERFUSU`=" . $idUsu; 
+			$queryUsu = mysql_query($sqlUsu, $conexao);
+			$resultUsu = mysql_fetch_array($queryUsu);
+
+			if ($resultUsu["TIPO_PERFUSU"] == 2) {
+		?>
+
+			<ul id="menu_colab">
+				<li class="menu_inicio">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
+						<p>Início</p>
+					</a>
+				</li>
+				<li class="menu_agenda">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+						<p>Agenda</p>
+					</a>
+				</li>
+				<li class="menu_pontos">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+						<p>Pontos</p>
+					</a>
+				</li>
+				<li class="menu_conquistas">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+						<p>Conquistas</p>
+					</a>
+				</li>
+				<li class="menu_relatorio">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+						<p>Relatório</p>
+					</a>
+				</li>
+			</ul>
+
+		<?php 
+		} else if ($resultUsu["TIPO_PERFUSU"] == 1) {
+		?>
+			<ul id="menu_admin">
+				<li class="menu_inicio">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 0, 23, 23))" alt="Início">					
+						<p>Início</p>
+					</a>
+				</li>
+				<li class="menu_agenda">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 30, 23, 23))" alt="Agenda">
+						<p>Agenda</p>
+					</a>
+				</li>
+				<li class="menu_equipe">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+						<p>Equipe</p>
+					</a>
+				</li>
+				<li class="menu_pontuacao">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+						<p>Pontuação</p>
+					</a>
+				</li>
+				<li class="menu_conquistas">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+						<p>Conquistas</p>
+					</a>
+				<li class="menu_comunicados">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+						<p>Comunicados</p>
+					</a>
+				</li>
+				<li class="menu_relatorios">
+					<a href="">
+						<img src="img/iconset.svg#svgView(viewBox(0, 56, 23, 23))" alt="Agenda">
+						<p>Relatórios</p>
+					</a>
+				</li>
+			</ul>
+		<?php } ?>
+	</section>
+	<section id="menu_superior">
+		<ul>
+			<li id="botao" class="menu_ergonomia">
+				<a href="">Ergonomia</a>
+			</li>
+			<li id="notificacoes"  class="menu_notificacoes">
+				<img src="img/iconset.svg#svgView(viewBox(4, 115, 18, 23))" alt="Agenda">
+				<p id="not_cont"><?php echo $qtdNot ?></p>
+			</li>
+			<li class="menu_ajuda">
+				<p>Ajuda</p>
+				<img src="img/iconset.svg#svgView(viewBox(7, 134, 11, 29))" alt="Agenda">
+			</li>
+			<li class="menu_usuario">
+				<p>Olá, <?php echo $nome; ?></p>
+				<img src="img/iconset.svg#svgView(viewBox(7, 134, 11, 29))" alt="Agenda">
+			</li>
+		</ul>
+	</section>	
+</section>
--- notificacoes.php
+++ notificacoes.php
@@ -7,22 +7,10 @@ session_start();//INICIO SESSAO
 $nome = $_SESSION['NOME_PERFUSU'];
 $idUsu = $_SESSION["ID_PERFUSU"];
 
-echo "bem vindo ". $nome; //ALTERAR
 
 $sqlUsu = "SELECT `TIPO_PERFUSU`, `ID_EMP` FROM `perfil_usuario` WHERE `ID_PERFUSU`=" . $idUsu; 
 $queryUsu = mysql_query($sqlUsu, $conexao);
 $resultUsu = mysql_fetch_array($queryUsu);
-
-
-$sqlNot = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 0"; //SELECIONA TODAS AS NOTIFICACOES NAO VISUALIZADAS
-$queryNot = mysql_query($sqlNot, $conexao);
-
-$qtdNot = 0;
-
-while ($resultNot = mysql_fetch_array($queryNot)){
-	$qtdNot += 1; //CONTA QUANTAS NOTIFICACOES NAO VISUALIZADAS O USUARIO TEM
-}
-
 
 
 $sqlNotVis = "SELECT `ID_NOTPLA`,`DESCRICAO_NOTPLA`, `DESTINATARIOS_NOTPLA`, `DATA_NOTPLA`, `ICONE_NOTPLA`, `COR_NOTPLA`, `STATUS_NOTPLA` FROM `notificacoes_plat` WHERE `DESTINATARIOS_NOTPLA` LIKE '%" . $idUsu . "%' AND `STATUS_NOTPLA` = 1"; //SELECIONA TODAS AS NOTIFICACOES VISUALIZADAS
@@ -43,7 +31,7 @@ $queryNotVis = mysql_query($sqlNotVis, $conexao);
 	<title>ADMIN - MINIMAMENTE</title>
 
 	<!-- ESTILOS -->
-	<link rel="stylesheet" href="CSS/estilo.css">
+	<link rel="stylesheet" type="text/css" href="css/master.css">
 
 	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
 
@@ -51,8 +39,17 @@ $queryNotVis = mysql_query($sqlNotVis, $conexao);
 </head>
 
 <body>
-
+	
+	<?php include 'master.php'; ?>
+	<script>
+		$(document).ready(function(){
+			$(".menu_inicio").css('font-weight', '500');
+		});
+	</script>
+	
 	<h1>Notificacoes</h1>
+
+
 
 	<a href="historicoNotificacoes.php?idUsu=">Ver Mais</a>
 	<?php echo $idUsu ?>
@@ -88,7 +85,7 @@ $queryNotVis = mysql_query($sqlNotVis, $conexao);
 		<h4><?php echo $resultNot2['DESCRICAO_NOTPLA']; ?>blah</h4>
 	</div>
 
-	<script type="text/javascript">
+	<!--script type="text/javascript">
 		
 		$(document).ready(function(){
 
@@ -128,7 +125,7 @@ $queryNotVis = mysql_query($sqlNotVis, $conexao);
 		});
 
 
-	</script>
+	</script-->
 
 </body>
 
