6681c1566075d7b43b061caf0ca46b069667e1f1
erikacs30 <erika.cs30@gmail.com>
2017-09-12 14:27:11 -0300

    120903

--- validaLogIn.php
+++ validaLogIn.php
@@ -11,10 +11,11 @@ if($valorEmail && $valorSenha != '') {
 			$sql = mysqli_query($conexao, "SELECT * FROM perfil_usuario WHERE EMAIL_PERFUSU ='$valorEmail'");//seleciona o banco dados login do email
 			$cont = mysqli_num_rows($sql);//cont recebe a a linha selecionada
 				while($linha = mysqli_fetch_array($sql)){
-					$senha_db = $linha["SENHA_PERFUSU"];	
-					$nomeusu = $linha["NOME_PERFUSU"];
-					$IDusu = $linha["ID_PERFUSU"];
-					$Tipo = $linha["TIPO_PERFUSU"];
+					$senhaDb = $linha["SENHA_PERFUSU"];	
+					$nomeUsu = $linha["NOME_PERFUSU"];
+					$sobrenomeUsu = $linha["SOBRENOME_PERFUSU"];
+					$idUsu = $linha["ID_PERFUSU"];
+					$tipo = $linha["TIPO_PERFUSU"];
 					
 					//echo $ativo;
 				}
@@ -29,9 +30,10 @@ if($valorEmail && $valorSenha != '') {
 						session_cache_expire(30); //expira em quanto tempo?
 						session_start(); //session_start(['read_and_close'=>1]); se nao alterar dados da sessao - se alterar usar session_commit()0
 						session_regenerate_id(); //re-do after profile update
-						$_SESSION["NOME_PERFUSU"]= $nomeusu; 
-						$_SESSION["ID_PERFUSU"] = $IDusu;
-						$_SESSION["TIPO_PERFUSU"] = $Tipo;
+						$_SESSION["NOME_PERFUSU"]= $nomeUsu; 
+						$_SESSION["SOBRENOME_PERFUSU"]= $sobrenomeUsu; 
+						$_SESSION["ID_PERFUSU"] = $idUsu;
+						$_SESSION["TIPO_PERFUSU"] = $tipo;
 						//SESSION SETUP END
 
 						if($Tipo == 1){
@@ -49,28 +51,4 @@ if($valorEmail && $valorSenha != '') {
 					}
 				}
 			} 
-
-					/*if($senha_db != $valorSenha){//se a senha não for igual a que o admim cadastrou
-					header("Location:denodelogin.php?erro=1");
-					}
-					
-					else{	
-						session_cache_expire(30); //expira em quanto tempo?
-						session_start();
-						$_SESSION["NOME_PERFUSU"]= $nomeusu; //aqui é pra deixar o email oupuxar o nome?
-						$_SESSION["ID_PERFUSU"] = $IDusu;
-
-						if($Tipo = 1){
-						header("location:usudash.php");	
-					} else {
-						header("location:admindash.php");	
-					}
-
-
-							//session_destroy();
-						}
-							
-				}
-		}*/
-
             ?>
